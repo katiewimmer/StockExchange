8233121bd0b9ecdc3b465493e953c1d99f75f943
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2021-03-03 10:50:23 -0500

    add comments

--- TradeOrder.java
+++ TradeOrder.java
@@ -1,4 +1,9 @@
-
+/**
+Provides a constructor for creating a TradeOrder object with parameters
+for the trader, symbol, buying, market price and price
+Includes several accessor methods for each object characteristic
+Authors: SS, KW, RM
+*/
 public class TradeOrder
 {
   private Trader trader;
@@ -7,43 +12,42 @@ public class TradeOrder
   private int numShares;
   private double price;
   
-  public TradeOrder(Trader t, String s, boolean b, boolean m, int n, double p)
+  public TradeOrder(Trader t, String s, boolean b, boolean m, int n, double p) //constructor
   {
-    trader = t;
+    trader = t; //sets each parameter to a field in the TradeOrder class
     stockSymbol = s;
     buying = b;
     marketPrice = m;
     price = p;
   }
   
-  public Trader getTrader()
+  public Trader getTrader() //accessor method for trader
   {
     return trader;
   }
   
-  public String getStockSymbol()
+  public String getStockSymbol() //accessor method for stock symbol
   {
     return stockSymbol;
   }
   
-  //if false, is selling
-  public boolean isBuying()
+  public boolean isBuying()  
   {
-    return buying;
+    return buying; //if false, is selling
   }
   
-  //if false, is limit price
+
   public boolean isMarketPrice()
   {
-    return marketPrice;
+    return marketPrice;  //if false, is limit price
   }
   
-  public int getNumShares()
+  public int getNumShares() //accessor methods for number of shares
   {
     return numShares;
   }
   
-  public double getPrice()
+  public double getPrice() //accessor method for price
   {
     return price;
   } 
