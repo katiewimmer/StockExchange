e1c136c887ee5f1bfc864dfcb6fae253f88fbb85
Ryan Mai <rmai21@andover.edu>
2021-03-03 17:05:28 -0500

    Updates Brokerage and Trader classes

--- Brokerage.java
+++ Brokerage.java
@@ -1,4 +1,105 @@
-public class Brokerage implements Login
+/**
+ * Class that resembles a brokerage. Brokerage has a TreeMap
+ * of all registered users and a TreeSet of logged-in users.
+ * Receives quote requests and trade orders from traders and
+ * relays them to StockExchange.
+ * 
+ * @author: Ryan Mai, Sima Shmuylovich, Katie Wimmer
+ */
+
+import java.util.*;
+
+public class Brokerage implements Login 
 {
-  
+
+    private TreeMap<String, Trader> traders;
+    private TreeSet<Trader> loggedInTraders;
+    private StockExchange stockExchange;
+
+    /**
+     * Constructor for Brokerage
+     * @param stockExchange
+     */
+    public Brokerage(StockExchange stockExchange) 
+    {
+        traders = new TreeMap<String, Trader>();
+        loggedInTraders = new TreeSet<Trader>();
+        this.stockExchange = stockExchange;
+    }
+
+    /**
+     * Gets quote for a specific stock based on inputted symbol
+     * @param symbol
+     * @param trader
+     */
+    public void getQuote(String symbol, Trader trader) 
+    {
+        stockExchange.processQuote(symbol);
+    }
+
+    /**
+     * Relays TradeOrder to StockExchange
+     * @param tradeOrder
+     */
+    public void placeOrder(TradeOrder tradeOrder) 
+    {
+        stockExchange.addOrder(tradeOrder);
+    }
+
+    // Login authorization
+
+    /**
+     * Checks if an inputted name and password matches conditions from
+     * Login.java. Adds user to the registered traders TreeMap if conditions
+     * are met. 
+     * @param name
+     * @param password
+     * @return int
+     */
+    public int addUser(String name, String password) 
+    {
+        Trader nextTrader = new Trader(this, name, password);
+        if(!(name.length() >= 4 && name.length() <= 10))
+            return -1;
+        if(!(password.length() >= 2 && password.length() <=10))
+            return -2;
+        if(traders.containsKey(name))
+            return -3;
+        traders.put(name, nextTrader);
+        return 0;
+
+    }
+
+    /**
+     * Checks if the inputted name and password match records in
+     * the registered traders TreeMap. Adds the trader to loggedInTradders
+     * TreeSet if the trader is registered and not already logged in.
+     * @param name
+     * @param password
+     * @return int
+     */
+    public int login(String name, String password)
+    {
+        Trader trader = traders.get(name);
+        if(!traders.containsKey(name))
+            return -1;
+        if(!traders.get( name ).getPassword().equals(password))
+            return -2;
+        if(loggedInTraders.contains(traders.get(name))) 
+            return -3;
+        trader.openTraderWindow();
+        if(!trader.hasMail())
+            trader.getMail("Welcome to SafeTrade!");
+        loggedInTraders.add(trader);
+        return 0;
+    }
+
+    /**
+     * Removes a trader from the loggedInTraders TreeSet
+     * @param trader
+     */
+    public void logout(Trader trader)
+    {
+        loggedInTraders.remove(trader);
+    }
 }
---
+++ Trader.java
@@ -0,0 +1,98 @@
+/**
+ * Depicts a trader. Includes a constructer that accepts a brokerage, name, and password.
+ * A trader can request quotes and place orders with the brokerage. It also receives
+ * mail in the mailbox, and it can tell the TraderWindow to display the mail.
+ * @author: Ryan Mai, Sima Shmuylovich, Katie Wimmer
+ */
+
+import java.util.*;
+
+public class Trader 
+{
+
+    private Brokerage brokerage;
+    private String name, password;
+    private TraderWindow window;
+    private Queue<String> mailbox;
+
+    public Trader(Brokerage brokerage, String name, String password) 
+    {
+        this.brokerage = brokerage;
+        this.name = name;
+        this.password = password;
+        mailbox = new LinkedList<String>();
+    }
+
+    /**
+     * Gets quote from brokerage
+     * @param symbol
+     */
+    public void getQuote(String symbol) 
+    {
+        brokerage.getQuote(symbol, this);
+    }
+
+    /**
+     * Directs brokerage to place an order
+     * @param tradeOrder
+     */
+    public void placeOrder(TradeOrder tradeOrder) 
+    {
+        brokerage.placeOrder(tradeOrder);
+    }
+
+    /**
+     * Quits the program and logs out as a trader
+     */
+    public void quit()
+    {
+        brokerage.logout(this);
+    }
+
+    // Accessors
+    public String getName() 
+    {
+        return name;
+    }
+
+    public String getPassword() 
+    {
+        return password;
+    }
+
+    // Mailbox
+    /**
+     * Receives mail
+     * @param mail
+     */
+    public void getMail(String mail)
+    {
+        mailbox.add(mail);
+        if(window != null)
+        {
+            while(!mailbox.isEmpty()) 
+            {
+                window.showMessage(mailbox.remove());
+            }
+        }
+    }
+
+    public boolean hasMail()
+    {
+        return !mailbox.isEmpty();
+    }
+
+    // TraderWindow
+    /**
+     * Opens TraderWindow for displaying mail
+     */
+    public void openTraderWindow() 
+    {
+        window = new TraderWindow(this);
+        while(!mailbox.isEmpty()) 
+        {
+            window.showMessage(mailbox.remove());
+        }
+    }
+
+}
