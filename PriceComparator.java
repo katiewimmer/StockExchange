5398384087f45ce0ecc79ecd52843c26b083f77e
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2021-03-03 10:43:14 -0500

    add comments

---
+++ PriceComparator.java
@@ -0,0 +1,44 @@
+/**
+Provides a no-args constructor that sets ascending to true and a constructor
+with one parameter that dictates if ascending is true or false
+Dictates the process of comparing two trade orders depending on
+whether ascending is true or false
+Authors: KW, SS, RM
+*/
+import java.util.Comparator;
+
+public class PriceComparator implements Comparator<TradeOrder>
+{
+	private boolean ascending;
+	
+	public PriceComparator(boolean order) // sets ascending to boolean value of the parameter
+	{
+		ascending = order;
+	}
+	
+	public PriceComparator() //default sets ascending to true
+	{
+		ascending = true;
+	}
+	
+	public int compare(TradeOrder one, TradeOrder two)
+	{
+		if(one.isMarketPrice() && two.isMarketPrice()) //both are the market price
+			return 0; //equal
+		
+		if(one.isMarketPrice() && !two.isMarketPrice()) //one is the market price and the other is the limit
+			return -1; //less than
+		
+		if(two.isMarketPrice() && !one.isMarketPrice()) //two is the market price and the other is the limit
+		{
+			return 1; //more than
+		}
+		
+		if(ascending) //returns it in ascending
+			return (int) (one.getPrice() - two.getPrice() * 100);
+		else //returns it in decending
+			return (int) (-one.getPrice() + (two.getPrice() * 100)); //
+	}
+	
+	
+}
