2d08b7aacbdaafe9b3d94dc8c5b27630b227fd80
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2021-03-04 00:38:49 -0500

    add comments/documentation

--- Trader.java
+++ Trader.java
@@ -1,8 +1,11 @@
 /**
- * Depicts a trader. Includes a constructer that accepts a brokerage, name, and password.
- * A trader can request quotes and place orders with the brokerage. It also receives
- * mail in the mailbox, and it can tell the TraderWindow to display the mail.
- * @author: Ryan Mai, Sima Shmuylovich, Katie Wimmer
+ * Provides a constructer for a Trader that accepts a brokerage, name, and password,
+ * accessor methods to request quotes and place orders with the brokerage.
+ * Provides methods for the mailbox and call to TraderWindow to display mail
+ *
+ * CSC630 Period 8 Ms. Litvin
+ * Date: March 4, 2021
+ * Authors: Katie Wimmer, Sima Shmuylovich, Ryan Mai
  */
 
 import java.util.*;
@@ -17,24 +20,23 @@ public class Trader implements Comparable<Trader>
 
     public Trader(Brokerage brokerage, String name, String password) 
     {
-        this.brokerage = brokerage;
-        this.name = name;
-        this.password = password;
-        mailbox = new LinkedList<String>();
+        this.brokerage = brokerage; // sets to particular brokerage object
+        this.name = name; // sets to name of user
+        this.password = password; // sets to password of user
+        mailbox = new LinkedList<String>(); //initalizes the mailbox as a LinkedList
     }
 
     /**
-     * Gets quote from brokerage
-     * @param symbol
+     * Returns brokerage for a 
+     * a given symbol
      */
-    public void getQuote(String symbol) 
+    public void getQuote(String symbol)
     {
         brokerage.getQuote(symbol, this);
     }
 
     /**
-     * Directs brokerage to place an order
-     * @param tradeOrder
+     * Directs brokerage to place a tradeOrder
      */
     public void placeOrder(TradeOrder tradeOrder) 
     {
@@ -49,50 +51,55 @@ public class Trader implements Comparable<Trader>
         brokerage.logout(this);
     }
 
-    // Accessors
-    public String getName() 
+
+    public String getName() //acccessor method for name
     {
         return name;
     }
 
-    public String getPassword() 
+    public String getPassword() //accessor method for password
     {
         return password;
     }
 
-    // Mailbox
-     /** Receives mail
-     * @param mail
+     /** 
+     * adds a messafe to a user's mailbox and
+     * displays all messages in a user's mailbox
      */
     public void receiveMessages(String mail)
     {
-        mailbox.add(mail);
+        mailbox.add(mail); //adds mail to the mailbox
         if(window != null)
         {
-            while(!mailbox.isEmpty()) 
+            while(!mailbox.isEmpty()) //traverses through mailbox
             {
-                window.showMessage(mailbox.remove());
+                window.showMessage(mailbox.remove()); //displays each message
             }
         }
     }
-
+    
+    /**
+    * Returns true is the mailbox has at least one message, false otherwise
+    */
     public boolean receiveMessages()
     {
         return !mailbox.isEmpty();
     }
 
-    // TraderWindow
     /**
-     * Opens TraderWindow for displaying mail
+     * Initializes TraderWindow for displaying mail
      */
     public void openTraderWindow() 
     {
         window = new TraderWindow(this);
-        while(!mailbox.isEmpty()) 
+        while(!mailbox.isEmpty()) //traverses mailbox
         {
-            window.showMessage(mailbox.remove());
+            window.showMessage(mailbox.remove()); //displays each message
         }
     }
+    /**
+    * overloaded compareTo method fro two Trader objects
+    */
     public int compareTo(Trader other)
     {
       return this.getName().compareToIgnoreCase(other.getName());
