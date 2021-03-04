4b353ae39ff909cc9de3dfd43d8df4f93ff202aa
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2021-03-04 00:26:32 -0500

    add comments/documentation

--- StockExchange.java
+++ StockExchange.java
@@ -1,17 +1,19 @@
 /**
- * This class keeps a HashMap of listed stocks, keyed by their symbols, 
- * and relays quote requests and trade orders to them. 
+ * Keeps a HashMap of listed stocks, keyed by their symbols, 
+ * and provides method for quote requests and 
+ * trade orders to them. 
  * 
  * CSC630 Period 8 Ms. Litvin
  * Date: March 4, 2021
  * Authors: Katie Wimmer, Sima Shmuylovich, Ryan Mai
 */
+
 import java.util.Map;
 import java.util.HashMap;
 
 public class StockExchange 
 {
- private Map<String, Stock> listedStocks;
+ private Map<String, Stock> listedStocks; // map of active stocks
  
  /**
   * Initializes a Map of listed stocks.
@@ -22,14 +24,14 @@ public class StockExchange
  }
  
  /**
-  * Returns the quote of the listed stock associated with the given symbol.
+  * Returns the quote of the listed stock associated with a given symbol
   */
  public String processQuote(String symbol)
  {
    Stock stock = listedStocks.get(symbol);
    if (stock != null)
-     return stock.getQuote();
-   return symbol + " not found";
+     return stock.getQuote(); //returns the quote of the stock
+   return symbol + " not found"; // if the symbol is not related to a stock
  }
 
  /**
@@ -38,7 +40,7 @@ public class StockExchange
  public void listStock(String symbol, String name, double price) 
  {
    Stock stock = new Stock(symbol, name, price); //creates new stock
-   listedStocks.put(symbol, stock); //adds it to the list
+   listedStocks.put(symbol, stock); // adds it to the list
  }
  
  public void addOrder(TradeOrder recipient) //places an order to a given recipient
