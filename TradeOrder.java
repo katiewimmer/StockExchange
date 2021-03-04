db8fd4443fe59b3510caf51bc3d487f6ca936ce9
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2021-03-03 19:21:13 -0500

    fix minor bugs

--- TradeOrder.java
+++ TradeOrder.java
@@ -1,85 +1,49 @@
-/**
- * This class represents a "data carrier" object used by other objects to pass 
- * the data about a trade order to each other. The fields and accessor methods 
- * correspond to the data entry fields in TraderWindow.java.
- * 
- * CSC630 Period 8 Ms. Litvin
- * Date: March 4, 2021
- * Authors: Katie Wimmer, Sima Shmuylovich, Ryan Mai
- */
 public class TradeOrder
 {
   private Trader trader;
-  private String symbol;
-  private boolean buyOrder, marketOrder;
+  private String stockSymbol;
+  private boolean buying, marketPrice;
   private int numShares;
   private double price;
   
-  /**
-   * Initializes a TradeOrder with answers to the following questions:
-   *    Which trader is placing the order?
-   *    What stock is the order being placed on?
-   *    Is the trader looking to buy or sell?
-   *    Is the order a market order or a limit order?
-   *    How many shares does the trader wish to buy/sell?
-   *    What price is the trader offering?
-   */
-  public TradeOrder(Trader trader, String symbol, boolean buyOrder,
-      boolean marketOrder, int numShares, double price)
+  public TradeOrder(Trader t, String s, boolean b, boolean m, int n, double p)
   {
-    this.trader = trader;
-    this.symbol = symbol;
-    this.buyOrder = buyOrder;
-    this.marketOrder = marketOrder;
-    this.numShares = numShares;
-    this.price = price;
+    trader = t;
+    stockSymbol = s;
+    buying = b;
+    marketPrice = m;
+    price = p;
   }
   
-  /**
-   * Returns the trader placing the order. 
-   */
   public Trader getTrader()
   {
     return trader;
   }
   
-  /**
-   * Returns the symbol of the stock the order is being placed on.
-   */
-  public String getSymbol()
+  public String getStockSymbol()
   {
-    return symbol;
+    return stockSymbol;
   }
   
-  /**
-   * Returns true if the trader wishes to buy, false if they wish to sell. 
-   */  
+  //if false, is selling
   public boolean isBuying()
   {
-    return buyOrder;
+    return buying;
   }
   
-  /**
-   * Returns true if the order is a market order, false if it is a limit order. 
-   */  
+  //if false, is limit price
   public boolean isMarketPrice()
   {
-    return marketOrder;
+    return marketPrice;
   }
   
-  /**
-   * Returns the number of shares the trader wishes to buy/sell. 
-   */  
   public int getNumShares()
   {
     return numShares;
   }
   
-  /**
-   * Returns the offered price. 
-   */ 
   public double getPrice()
   {
     return price;
-  }
+  } 
 }
